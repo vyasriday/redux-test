1c558dd4457c9ae51089c0668164a7f3bf34589d
Hridayesh Sharma <vyasriday7@gmail.com>
2019-05-08 19:33:54 +0530

    redux basics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createStore } from 'redux'; 
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,7 +16,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
       </header>
@@ -24,3 +25,40 @@ function App() {
 }
 
 export default App;
+
+// this is our initial state
+const defaultState = {
+  welcome: 'Hi',
+  otherState: []
+};
+/* 
+* this is the reducer that takes the action and current state and returns a new state object 
+* based on the action dispatched by dispatcher. We pass initial state to it.
+*/
+const greeting = (state = defaultState, action) => {
+ 
+  switch(action.type) {
+    case 'GREET_ME': 
+      return {...state, welcome: `Hello ${action.name}`};
+    case 'GREET_WORLD': 
+      return {...state, welcome: 'Hello World'};
+    default: 
+      return state;
+  }
+}
+// This is our store created using the reducer function
+const store = createStore(greeting);
+
+// getting our state before dispatching an action
+console.log(store.getState());
+
+const name = 'Hridayesh';
+
+// this is the dispatcher that takes and object with action type in it
+store.dispatch({
+  type: 'GREET_ME',
+  name
+})
+
+console.log(store.getState());
+
